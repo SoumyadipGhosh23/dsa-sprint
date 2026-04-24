a2e6d9567a7feded62c6f156d79cd40ea35105fe
SoumyadipGhosh23 <gsoumyadip2307@gmail.com>
2026-04-25 00:40:20 +0530

    feat: add recursive dynamic programming solution for minimum sum partition problem

---
+++ dp/05_minimumSumPartition.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+class Solution {
+    subsetSum(arr, n, sum, dp) {
+        if (sum == 0) return true;
+        if (n == 0 && sum > 0) return false;
+
+        if (dp[n][sum] !== -1) return dp[n][sum];
+
+        if (arr[n - 1] <= sum) return dp[n][sum] = this.subsetSum(arr, n - 1, sum - arr[n - 1], dp) || this.subsetSum(arr, n - 1, sum, dp);
+        else return dp[n][sum] = this.subsetSum(arr, n - 1, sum, dp);
+    }
+    minDifference(arr) {
+        // your code here
+        let sum = 0;
+        let n = arr.length;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        let dp = []
+        for (let i = Math.floor(sum / 2); i >= 0; i--) {
+            dp = Array.from({ length: n + 1 }, () => Array(i + 1).fill(-1));
+            if (this.subsetSum(arr, n, i, dp))
+                return (sum - i) - i;
+        }
+    }
+}
